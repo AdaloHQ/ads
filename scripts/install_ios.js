a52c4574432deaeb5f2572f2c734d88ad5f6105c
achowksey <achowksey@wustl.edu>
2020-07-11 20:20:28 -0400

    ios script

--- scripts/install_ios.js
+++ scripts/install_ios.js
@@ -1,5 +1,6 @@
+// get ios App ID
 const fs = require('fs')
-let rawData = fs.readFileSync('../../packager/builds/AdaloApp/proton-bundle.json');
+let rawData = fs.readFileSync('../../template-app/proton-bundle.json');
 let parsedData = JSON.parse(rawData);
 let stringData = JSON.stringify(parsedData);
 let i = stringData.indexOf('"' + "iosAppID" + '":')+12
@@ -11,21 +12,16 @@ while(i < stringData.indexOf('"' + "iosAppID" + '":')+100){
 let iosAppID = string.split('"')
 iosAppID = iosAppID[0]
 appID = iosAppID.replace(/\s/g,'')
-try {
-    process.chdir('../../template-app/ios/TavoloRestaurant');
-    console.log('New directory: ' + process.cwd());
-}
-catch (err) {
-    console.log('chdir: ' + err);
-}
-const { exec } = require('child_process');
 
+// add iOS App ID to info.plist
+process.chdir('../../template-app/ios/TavoloRestaurant');
+const { exec } = require('child_process');
 exec('plutil -remove GADApplicationIdentifier info.plist', () => {
     exec('plutil -insert GADApplicationIdentifier -string ' + appID + ' info.plist', (err) => {
         if (err) {
           console.error('error: ' + err)
         } else {
-         console.log(`added!`);
+         console.log(`iOS App ID added!`);
         }
       });
   });
---
+++ scripts/install_ios.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+set -x
+
+name=$PROJECT_NAME
+
+# Frameworks
+cp -R ../GoogleMobileAds.framework ../../template-app/ios
+
+# AdMob Dependencies
+cd ../../template-app
+
+yarn add react-native-admob@^2.0.0-beta.6
+# yarn add ads@../ads
+
+# Podfile
+cd ios
+
+sed -i.bak '/use_native_modules/a\
+  pod "Google-Mobile-Ads-SDK" 
+' Podfile
+
+pod install --repo-update
+
+# AppDelegate.m
+sed -i.bak '/UserNotifications.h/a\
+  @import GoogleMobileAds;
+' ./TavoloRestaurant/AppDelegate.m
+
+# sed -i.bak '/center.delegate = self;/i\
+# GADMobileAds.sharedInstance().start(completionHandler: nil)
+# ' ./TavoloRestaurant/AppDelegate.m
+
+echo "configured iOS settings"
