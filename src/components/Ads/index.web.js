b2d1cd384fb4c2106862ea711d96bc796498fe54
Scott Massey <scott.massey@wustl.edu>
2020-07-30 13:20:19 -0500

    fixed issue with placeholder images

--- src/components/Ads/index.web.js
+++ src/components/Ads/index.web.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react'
-import { View, Image } from 'react-native';
-import * as stdAd from './adPics/stdAd.png'
-import * as largeAd from './adPics/largeAd.png'
-import * as recAd from './adPics/recAd.png'
+import React, { Component } from "react";
+import { View, Image } from "react-native";
+import stdAd from "./adPics/stdAd.png";
+import largeAd from "./adPics/largeAd.png";
+import recAd from "./adPics/recAd.png";
 
 class Ads extends Component {
-	render() {
-		const { size } = this.props
-		let height = 250
-		let width = 300
-		let image = recAd
-		if (size == 'banner'){
-			height = 50
-			width = 320
-			image = stdAd
-		}
-		else if (size == 'largeBanner'){
-			height = 100
-			width = 320
-			image = largeAd
-		}
-		let realStyles = {
-			wrapper: {
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				height: height,
-				width: width
-			},
-			image: {
-				height: height,
-				width: width
-			}
-		}
-		return (
-			<View style={realStyles.wrapper}>
-				<Image style={realStyles.image} source={image} />
-			</View>
-		)
-	}
+  render() {
+    const { size } = this.props;
+    let height = 250;
+    let width = 300;
+    let image = recAd;
+    if (size == "banner") {
+      height = 50;
+      width = 320;
+      image = stdAd;
+    } else if (size == "largeBanner") {
+      height = 100;
+      width = 320;
+      image = largeAd;
+    }
+    let realStyles = {
+      wrapper: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: height,
+        width: width,
+      },
+      image: {
+        height: height,
+        width: width,
+      },
+    };
+    return (
+      <View style={realStyles.wrapper}>
+        <img style={realStyles.image} src={image} />
+      </View>
+    );
+  }
 }
 
-export default Ads
+export default Ads;
