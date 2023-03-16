b3428513d1eee9c636a1e1d8847ddc86d18d55be
Guislain Bonnard <guislain@adalo.com>
2023-03-16 15:54:02 -0600

    v2.0 admob rewrite

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -12,53 +12,7 @@ if [ -z "$appID" ]; then
   exit 0;
 fi
 
-# AdMob Dependencies
-yarn add react-native-admob@https://github.com/AdaloHQ/react-native-admob.git
-
-# build.gradle
-cd android/app
-
-sed -i.bak '/dependencies {/a\
-implementation "com.android.support:multidex:1.0.3"\
-implementation "com.google.android.gms:play-services-ads:21.5.0"\
-implementation "com.google.android.gms:play-services-base:18.2.0"\
-implementation "com.google.firebase:firebase-core:21.1.1"\
-implementation "com.google.firebase:firebase-messaging:23.1.2"\
-implementation "androidx.work:work-runtime-ktx:2.7.1"
-' build.gradle
-
-# AndroidManifest
-cd src/main
-cat <<EOF > /tmp/adalo-sed
-/com.facebook.react.devsupport/a\\
-    <meta-data android:name="com.google.android.gms.ads.APPLICATION_ID" android:value="${appID}"/>\\
-EOF
-
-sed -i.bak "$(cat /tmp/adalo-sed)" AndroidManifest.xml
-
-echo $BUNDLE_ID
-app_path=$(echo ${BUNDLE_ID} | sed -e 's/\./\//g')
-
-# MainActivity
-cd java/$app_path
-
-sed -i.bak '/ReactActivity;/a\
-import android.os.Bundle;\
-import com.google.android.gms.ads.MobileAds;\
-import com.google.android.gms.ads.initialization.InitializationStatus;\
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-' MainActivity.java
-
-sed -i.bak '/public class MainActivity extends ReactActivity {/a\
-@Override\
-protected void onCreate(Bundle savedInstanceState) {\
-  super.onCreate(savedInstanceState);\
-  MobileAds.initialize(this, new OnInitializationCompleteListener() {\
-    @Override\
-    public void onInitializationComplete(InitializationStatus initializationStatus) {\
-    }\
-  });\
-}\
-' MainActivity.java
-
-echo "configured Android settings"
+sed -i.bak 's/^{/{\
+  "react-native-google-mobile-ads": {\
+    "android_app_id": "'$appId'"\
+  },'/g $project_path/app.json
--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -1,109 +1,85 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, Platform } from 'react-native'
-import { AdMobBanner, AdMobInterstitial } from 'react-native-admob'
+import mobileAds, {
+  InterstitialAd,
+  BannerAd,
+  BannerAdSize,
+} from 'react-native-google-mobile-ads'
+
+const sizesMap = {
+  banner: BannerAdSize.BANNER,
+  largeBanner: BannerAdSize.LARGE_BANNER,
+  mediumRectangle: BannerAdSize.MEDIUM_RECTANGLE,
+  interstitial: null,
+}
 
 const Ads = props => {
-  let {
-    iosAdID,
-    andAdID,
-    size,
-    iosAppIDGlobal,
-    iosAppID,
-    andAppIDGlobal,
-    andAppID,
-  } = props
+  let { iosAdID, andAdID, iosAppIDGlobal, andAppIDGlobal, size } = props
 
-  if (
-    (Platform.OS === 'ios' && !(iosAppIDGlobal || iosAppID)) ||
-    (Platform.OS === 'android' && !(andAppIDGlobal || andAppID))
-  ) {
-    const platformPretty = Platform.OS === 'ios' ? 'iOS' : 'Android'
+  const isAndroid = Platform.OS === 'android'
+
+  if ((!isAndroid && !iosAppIDGlobal) || (isAndroid && !andAppIDGlobal)) {
     return (
       <View style={styles.errorView}>
         <Text style={styles.errorText}>
-          Component Setup Incomplete. {platformPretty} App ID was left blank.
+          Component Setup Incomplete. {isAndroid ? 'Android' : 'iOS'} App ID was
+          left blank.
         </Text>
       </View>
     )
   }
 
-  const [adID, setAdID] = useState('')
+  if ((!isAndroid && !iosAdID) || (isAndroid && !andAdID)) {
+    return (
+      <View style={styles.errorView}>
+        <Text style={styles.errorText}>
+          Component Setup Incomplete. {isAndroid ? 'Android' : 'iOS'} Ad ID was
+          left blank.
+        </Text>
+      </View>
+    )
+  }
 
-  const logError = error => {
-    console.error('Error loading ad!:', error)
+  const adId = (isAndroid ? andAdID : iosAdID).replace(/\s/g, '')
+
+  const [initializing, setInitializing] = useState(false)
+  const [initialized, setInitialized] = useState(false)
+
+  useEffect(async () => {
+    if (initializing || initialized) return
+
+    setInitializing(true)
+    const adapterStatuses = await mobileAds().initialize()
+    console.log(adapterStatuses)
+    setInitialized(true)
+  })
+
+  if (!initialized) {
+    return (
+      <View style={styles.errorView}>
+        <Text style={styles.errorText}>Initializing...</Text>
+      </View>
+    )
   }
 
   useEffect(() => {
-    let adIdLocal
-    if (Platform.OS === 'ios') {
-      if (iosAdID) {
-        adIdLocal = iosAdID.replace(/\s/g, '')
-        setAdID(adIdLocal)
-        if (size === 'interstitial') {
-          showInterstitial(adIdLocal)
-        }
-      }
-    } else if (Platform.OS === 'android') {
-      if (andAdID) {
-        let adIdLocal = andAdID.replace(/\s/g, '')
-        setAdID(adIdLocal)
-        if (size === 'interstitial') {
-          showInterstitial(adIdLocal)
-        }
-      }
-    }
-  }, [Platform.OS, iosAdID, andAdID])
+    if (size !== 'interstitial') return
 
-  const showInterstitial = adIdLocal => {
-    AdMobInterstitial.setAdUnitID(adIdLocal)
-    AdMobInterstitial.requestAd()
-      .then(() => {
-        AdMobInterstitial.showAd().catch(error =>
-          console.warn(error + ' error showing ad')
-        )
-      })
-      .catch(error => {
-        console.warn(error + ' ERROR REQUESTING AD')
-        logError(error)
-      })
-  }
-
-  useEffect(() => {
-    if (size === 'interstitial') {
-      AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId])
-
-      AdMobInterstitial.addEventListener('adLoaded', () =>
-        console.log('AdMobInterstitial adLoaded')
-      )
-      AdMobInterstitial.addEventListener('adFailedToLoad', error => {
-        console.warn('Ad failed to load!', error)
-        logError(error)
-      })
-      AdMobInterstitial.addEventListener('adOpened', () =>
-        console.log('AdMobInterstitial => adOpened')
-      )
-      AdMobInterstitial.addEventListener('adClosed', () => {
-        console.log('AdMobInterstitial => adClosed')
-      })
-      AdMobInterstitial.addEventListener('adLeftApplication', () =>
-        console.log('AdMobInterstitial => adLeftApplication')
-      )
-
-      return function cleanup() {
-        AdMobInterstitial.removeAllListeners()
-        console.log('ad gone?')
-      }
-    }
-  }, [size])
+    const interstitial = InterstitialAd.createForAdRequest(adId, {
+      requestNonPersonalizedAdsOnly: true,
+    })
+    interstitial.load()
+  })
 
   return (
     <View style={styles.wrapper}>
-      {size !== 'interstitial' && !!adID && (
-        <AdMobBanner
-          adSize={size}
-          adUnitID={adID}
-          onAdFailedToLoad={error => logError(error)}
-          testDevices={[AdMobBanner.simulatorId]}
+      {size !== 'interstitial' && !!adId && (
+        <BannerAd
+          unitId={adId}
+          size={sizesMap[size]}
+          requestOptions={{
+            requestNonPersonalizedAdsOnly: true,
+          }}
         />
       )}
     </View>
