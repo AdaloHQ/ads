01e2c957bc872f2b598f7c3e0001d401108fd6a9
Guislain Bonnard <guislain@adalo.com>
2023-03-20 10:42:52 -0400

    Simplify install script

--- scripts/parseJSON.js
+++ scripts/parseJSON.js
@@ -1,52 +1,20 @@
 #!/usr/bin/env node
 
-// Util function to do a DFS on the proton bundle
-const traverseObject = (obj, callback) => {
-  // Array
-  if (Array.isArray(obj)) {
-    return obj.map(child => traverseObject(child, callback))
-  }
-
-  // Non-object
-  if (!obj || typeof obj !== 'object') {
-    return obj
-  }
-
-  for (let child of Object.values(obj)) {
-    traverseObject(child, callback)
-  }
-
-  callback(obj)
-}
-
 const [, , ...args] = process.argv
 if (args.length !== 2) {
   console.error(
-    'Usage:\n\n node install_android.js [PROJECT_PATH] [PLATFORM_ID]\n'
+    'Usage:\n\n node parseJSON.js [PROJECT_PATH] [PLATFORM_ID]\n'
   )
   process.exit(1)
 }
-let project_path = args[0]
-let platform_id = args[1]
-let fs = require('fs')
-let rawData = fs.readFileSync(project_path + '/proton-bundle.json')
-let protonBundle = JSON.parse(rawData)
-let appID
-appID = protonBundle.libraryGlobals['@adalo/ads']['Ads'][`${platform_id}Global`]
+const project_path = args[0]
+const platform_id = args[1]
+const protonBundle = require(project_path + '/proton-bundle.json')
+const appID = protonBundle.libraryGlobals['@adalo/ads']['Ads'][`${platform_id}`]
 
-if (appID) {
-  let appIDFinal = appID.replace(/\s/g, '')
-  console.log(appIDFinal)
-} else {
-  traverseObject(protonBundle, node => {
-    if (node[platform_id]) {
-      appID = node[platform_id]
-    }
-  })
-  if (typeof appId !== 'undefined') {
-    let appIDFinal = appID.replace(/\s/g, '')
-    console.log(appIDFinal)
-  } else {
-    console.log()
-  }
-}
+if (!appID) {
+  console.error(`Could not find ${platform_id} in ${project_path}/proton-bundle.json` )
+  process.exit(1)
+} 
+
+console.log(appID.replace(/\s/g, ''))
